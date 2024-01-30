7e6b7c4c91ca9f4ddd567aa4bf10898ac60dd1c7
Nina Zhou <nzhou@lcs.on.ca>
2024-01-30 11:31:15 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Shuyu Zhou on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
